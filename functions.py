048dccebd1365d02b82a1ab597cd00a8aa882057
jonathan <j.guyot-bourgeois@libr.im>
2021-09-04 01:51:41 +0200

    Ajout du scrap de l'url de l'image

--- functions.py
+++ functions.py
@@ -5,8 +5,27 @@ from bs4 import BeautifulSoup
 def product_scrap(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
+
     title = soup.find("div", class_="product_main").find("h1")
+
     stock = soup.find("p", class_="availability")
-    description = soup.find("article", class_="product_page").find("p", recursive=False)
+
+    description = soup.find("article", class_="product_page").find(
+            "p", recursive=False)
+
     upc = soup.find("td")
-    return url, title, stock, description, upc
+
+    img = soup.find("div", class_="thumbnail").find("img")
+    img = img['src'].replace('../../', '')
+    img_url = "https://books.toscrape.com/" + img
+
+    product_list = [
+            url,
+            title.string,
+            stock.string,
+            description.string,
+            upc.string,
+            img_url
+            ]
+
+    return product_list
