a82838d069a3c74afee7db4c57c9be6eb9a63e76
jonathan <j.guyot-bourgeois@libr.im>
2021-09-04 00:18:58 +0200

    Ajout du scrap de la description

--- functions.py
+++ functions.py
@@ -7,5 +7,6 @@ def product_scrap(url):
     soup = BeautifulSoup(page.content, "html.parser")
     title = soup.find("div", class_="product_main").find("h1")
     stock = soup.find("p", class_="availability")
+    description = soup.find("article", class_="product_page").find("p", recursive=False)
     upc = soup.find("td")
-    return url, title, stock, upc
+    return url, title, stock, description, upc
