1b89145bb11cf4859e78c88609f9aa8ec5e57ba2
jonathan <j.guyot-bourgeois@libr.im>
2021-09-05 00:02:31 +0200

    Ajout du scrap de la notation

--- functions.py
+++ functions.py
@@ -10,21 +10,34 @@ def product_scrap(url):
     description = soup.find("article", class_="product_page").find(
             "p", recursive=False)
     img = soup.find("div", class_="thumbnail").find("img")
-    img = img['src'].replace('../../', '')
-    img_url = "https://books.toscrape.com/" + img
+    img = img['src'].replace('../../', 'https://books.toscrape.com/')
     product_info = soup.find("table", class_="table-striped").find_all("td")
     upc = product_info[0].string
     price_et = product_info[2].string
     price_it = product_info[3].string
     stock = product_info[5].string
+    rating = str(soup.find("p", class_="star-rating"))
+    if ("One") in rating:
+        rating = "1/5"
+    elif ("Two") in rating:
+        rating = "2/5"
+    elif ("Three") in rating:
+        rating = "3/5"
+    elif ("Four") in rating:
+        rating = "4/5"
+    elif ("Five") in rating:
+        rating = "5/5"
+    else:
+        rating = None
     product_list = [
             title.string,
             description.string,
+            rating,
             price_et,
             price_it,
             upc,
             stock,
-            img_url,
+            img,
             url
             ]
     return product_list
