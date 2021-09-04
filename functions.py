683fa2f46b81225764793cd0324555f057826b4a
jonathan <j.guyot-bourgeois@libr.im>
2021-09-04 23:02:58 +0200

    Amélioration

--- functions.py
+++ functions.py
@@ -3,32 +3,28 @@ from bs4 import BeautifulSoup
 
 
 def product_scrap(url):
+    """Scrap data from a product"""
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
-
     title = soup.find("div", class_="product_main").find("h1")
-
     description = soup.find("article", class_="product_page").find(
             "p", recursive=False)
-
     img = soup.find("div", class_="thumbnail").find("img")
     img = img['src'].replace('../../', '')
     img_url = "https://books.toscrape.com/" + img
-
+    product_info = soup.find("table", class_="table-striped").find_all("td")
+    upc = product_info[0].string
+    price_et = product_info[2].string
+    price_it = product_info[3].string
+    stock = product_info[5].string
     product_list = [
-            url,
             title.string,
             description.string,
-            img_url
+            price_et,
+            price_it,
+            upc,
+            stock,
+            img_url,
+            url
             ]
-
-    product_info = soup.find("table", class_="table-striped").find_all("td")
-    i = 0
-    for tds in product_info:
-        if i <= 5 and i != 1 and i != 4:
-            product_list.append(product_info[i].string)
-            i += 1
-        else:
-            i += 1
-
     return product_list
