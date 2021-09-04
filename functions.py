e9df42c5002f441702486adebfac124abe2f831b
jonathan <j.guyot-bourgeois@libr.im>
2021-09-05 01:39:28 +0200

    Ajout du scrap de la catégorie du livre

--- functions.py
+++ functions.py
@@ -7,6 +7,8 @@ def product_scrap(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     title = soup.find("div", class_="product_main").find("h1")
+    category = soup.find("ul", class_="breadcrumb").find_all("a")
+    category = category[2].string
     description = soup.find("article", class_="product_page").find(
             "p", recursive=False)
     img = soup.find("div", class_="thumbnail").find("img")
@@ -19,6 +21,7 @@ def product_scrap(url):
     rating = product_rating(str(soup.find("p", class_="star-rating")))
     product_list = [
             title.string,
+            category,
             description.string,
             rating,
             price_et,
