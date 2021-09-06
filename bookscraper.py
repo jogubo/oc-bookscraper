c36e30605a10d4c88fee7b9ad6e3657039302691
jonathan <j.guyot-bourgeois@libr.im>
2021-09-06 19:40:13 +0200

    Correction de bug

--- bookscraper.py
+++ bookscraper.py
@@ -5,10 +5,10 @@ page_url = category_url
 page_nb = 1
 
 while page_url is not None:
-    page_url = fct.page(category_url, page_nb)
     books = fct.books_links(page_url)
     i = 0
     for book in books:
         fct.product_infos(books[i])
         i += 1
+    page_url = fct.page(category_url, page_nb)
     page_nb += 1
